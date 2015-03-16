5ffcc610417927ac0357de1b152b31a93f723ee9
aristeuroriz <aristeuroriz@gmail.com>
2015-03-16 17:55:41 -0300

    seo-optmus-cont

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  resources :academia
-
-  resources :exercicios
-
-  resources :tp_exercicios
+#  resources :academia
 
   resources :treinos
 
-  resources :planos
+ # resources :planos
 
   resources :medidas
 
@@ -20,8 +16,7 @@ Rails.application.routes.draw do
 
   devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  
-    root 'home#index'
+   root 'home#index'
 
 
 end
