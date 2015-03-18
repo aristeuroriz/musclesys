131cf4221901c097e66151a8015a488de3c79462
aristeuroriz <aristeuroriz@gmail.com>
2015-03-17 22:46:25 -0300

    devise-confirm

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :treinos, dependent: :destroy
 
   # Include default devise modules. Others available are:
-  #  :lockable, :timeoutable and :omniauthable :confirmable,
-  devise  :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable, :timeoutable, :omniauthable, :omniauth_providers => [:facebook], :timeout_in => 15.minutes
+  #  :lockable, :timeoutable and :omniauthable 
+  devise  :confirmable, :database_authenticatable, :registerable,:recoverable, :rememberable, :trackable, :validatable, :timeoutable, :omniauthable, :omniauth_providers => [:facebook], :timeout_in => 15.minutes
 
   def self.from_omniauth(auth)
   	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
