a2d082a218cdfe8298f4c72556da377b4b7d0ed9
aristeuroriz <aristeuroriz@gmail.com>
2015-04-14 14:11:06 -0300

    redirect-routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,12 @@ match 'contact' => 'contact#create', :via => :post
  match 'painel/index'  => 'painel#index' , via: [:get], as: :painel
 
  devise_for :users,  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks",
- 	registrations: 'registrations' },  :path_names => {:sign_in => 'login', :sign_out => 'logout', :sign_up => 'signup'}
+ 	registrations: 'registrations' },  :path_names => {:sign_in => 'login', :sign_out => 'logout',
+ 		:sign_up => 'signup'}
+
+ 		match "/users/sign_in", :to => redirect("/users/login"), via: [:get]
+ 		match "/users/sign_out", :to => redirect("/users/logout"), via: [:get]
+ 		match "/users/sign_up", :to => redirect("/users/signup"), via: [:get]
 
  	root 'home#index'
 
