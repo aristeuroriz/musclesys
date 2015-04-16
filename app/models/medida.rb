9e6781e0bd7c3ce75fb28cabf1141dc9dcb64bdf
aristeuroriz <aristeuroriz@gmail.com>
2015-04-15 21:56:14 -0300

    debug-database

--- app/models/medida.rb
+++ app/models/medida.rb
@@ -4,10 +4,14 @@ class Medida < ActiveRecord::Base
 
 	def self.personal
 
-	query = "UPDATE medidas m, users u SET m.personal_id = u.personal_id WHERE m.user_id = u.id"
-	
-	results = ActiveRecord::Base.connection.execute(query)
+		@medida = Medida.all
 
+		if not @medida.blank?
+
+			query = "UPDATE medidas m, users u SET m.personal_id = u.personal_id WHERE m.user_id = u.id"
+			
+			results = ActiveRecord::Base.connection.execute(query)
+		end
 	end
 
 end
