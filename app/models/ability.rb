e24dd89c073db1e63a0eed82ced1eac8ffc2216f
aristeuroriz <aristeuroriz@gmail.com>
2015-06-12 17:31:09 -0300

    Clean class Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,7 @@
 class Ability
   include CanCan::Ability
 
-  user ||= User.new
-
-  alias_action :create, :read, :update, :destroy, :to => :crud
-
-  alias_action :create, :read, :update, :to => :prof
-
     def initialize(user)
-
-        if user.admin?
-          can :manage, :all
-        end
 
     end
 
