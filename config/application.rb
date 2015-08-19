f3e3ae9e8fafdd3ca8909a823ac61908f8bdbd56
Aristeu Roriz <aristeuroriz@gmail.com>
2015-08-19 17:08:32 -0300

    config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -28,6 +26,7 @@ module Musclesys
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.initialize_on_precompile = false
     
     config.assets.precompile += ['gpizza.js', 'mapa.js', 'gimc.js', 'glinha.js', 'gcol.js', 'alert.js', 'bemvindo.js']   
 
